6ee4b543a5677b38363aba181313d92f1976acce
ZEvans1 <zte.zachary@gmail.com>
2018-09-17 23:21:27 -0700

    Change minSDK to 17 from 15. Update README to explain change.

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ A game to see how long you can go with your phone as a brick
 * Android
 * Kotlin
 * Anko
+
+#### Notes
+* Decided to change min API to 17
+    * 15 and 16 only account for 1.5% of distribution
+    * According to https://developer.android.com/about/dashboards/
--- app/src/main/java/com/example/zachevans/softbrick/MainActivity.kt
+++ app/src/main/java/com/example/zachevans/softbrick/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.zachevans.softbrick
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
@@ -12,6 +13,12 @@ class MainActivity : AppCompatActivity() {
 
         verticalLayout {
             padding = dip(30)
+
+            textView {
+                text = "Softbrick"
+                textSize = 30f
+                textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
 
             button(R.string.start_button_text){
                 textSize = 26f
