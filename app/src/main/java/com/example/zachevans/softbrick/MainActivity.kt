062c6128fc5174e4a7667b248179218da568324e
ZEvans1 <zte.zachary@gmail.com>
2018-09-17 23:01:17 -0700

    Add start, stop, and reset buttons.

--- app/src/main/java/com/example/zachevans/softbrick/MainActivity.kt
+++ app/src/main/java/com/example/zachevans/softbrick/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.zachevans.softbrick
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        verticalLayout {
+            padding = dip(30)
+
+            button("Start"){
+                textSize = 26f
+                onClick {
+                    longToast("Start pressed")
+                }
+            }
+            button("Stop"){
+                textSize = 26f
+                onClick {
+                    longToast("Stop pressed")
+                }
+            }
+            button("Reset"){
+                textSize = 26f
+                onClick {
+                    longToast("Reset pressed")
+                }
+            }
+        }
     }
 }
